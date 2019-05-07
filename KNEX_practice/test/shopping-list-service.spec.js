896e4054a15510d74df346527b3de6368371a58e
Peter Pae <paendabear@gmail.com>
2019-05-07 17:00:10 -0400

    add files and base structure for TDD service building also add context for tests

---
+++ KNEX_practice/test/shopping-list-service.spec.js
@@ -0,0 +1,75 @@
+const ShoppingListService = require('../src/shopping-list-service')
+const knex = require('knex')
+
+describe('shopping list service object', function() {
+    let db;
+    let testItems = [
+        {
+            id: 1,
+            name: 'test1',
+            price: '1.00',
+            Category: 'Main'
+        },
+        {
+            id: 2,
+            name: 'test2',
+            price: '2.00',
+            Category: 'Snack'
+        },
+        {
+            id: 3,
+            name: 'test3',
+            price: '3.33',
+            Category: 'Breakfast'
+        },
+        {
+            id: 4,
+            name: 'test4',
+            price: '4.44',
+            Category: 'Lunch'
+        },
+        {
+            id: 5,
+            name: 'test5',
+            price: '5.55',
+            Category: 'Main'
+        }
+    ];
+
+    before(() => db('shopping_list').truncate())
+
+    afterEach(() => db('shopping_list').truncate())
+
+    after(() => db.destroy())
+
+    context(`Given 'shopping_list' has data`, () => {
+        beforeEach(() => {
+            return db
+                .into('shopping_list')
+                .insert(testItems)
+            })
+        })
+
+        it(`getAllItems() returns all items from 'shopping_list' table`, () => {
+            
+            return
+        } )
+
+        it(`deleteItem() removes item by id from 'shopping_list' table`, () => {
+
+            return
+        })
+
+        it(`updateItem() updates item by id with new fields from 'shopping_list' table`, () => {
+
+            return
+        })
+        it(`insertItem() inserts item with new specified id into 'shopping_list' table`, () => {
+
+            return
+        })
+        it(`getById() returns an item with the specified id from 'shopping_list' table` () => {
+
+            return
+        })
+})
