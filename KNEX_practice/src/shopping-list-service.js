4f0940d95771266eed942276a9ae764f31033b2b
Peter Pae <paendabear@gmail.com>
2019-05-08 12:30:29 -0400

    finish making service with tests

--- KNEX_practice/src/shopping-list-service.js
+++ KNEX_practice/src/shopping-list-service.js
@@ -1,21 +1,47 @@
 const ShoppingListService = {
-    getAllItems() {
+    getAllItems(knex) {
         return knex
                 .select('*')
                 .from('shopping_list')
     },
     deleteItem(knex, id){
-        return
+        return knex('shopping_list')
+                .where({ id })
+                .delete()
     },
     updateItem(knex, id, newFields){
-        return
+        return knex('shopping_list')
+                .where({id})
+                .update(newFields)
     },
     insertItem(knex, newItem){
-        return
+        return knex
+                .insert(newItem)
+                .into('shopping_list')
+                .returning('*')
+                .then(rows => rows[0])
     },
     getById(knex, id){
-        return 
+        return knex 
+                .from('shopping_list')
+                .select('*')
+                .where('id',id)
+                .first()
     },
 }
 
 module.exports = ShoppingListService;
+
+// {
+//     id: 3,
+//     name: newFields.name,
+//     price: newFields.price,
+//     checked: newFields.checked,
+//     category: newFields.category
+// }
+
+// {
+//     id: targetId,
+//     ...originalItem,
+//     ...newFields
+// }
--- KNEX_practice/test/shopping-list-service.spec.js
+++ KNEX_practice/test/shopping-list-service.spec.js
@@ -1,3 +1,4 @@
+
 const ShoppingListService = require('../src/shopping-list-service')
 const knex = require('knex')
 
@@ -8,33 +9,40 @@ describe('shopping list service object', function() {
             id: 1,
             name: 'test1',
             price: '1.00',
-            Category: 'Main'
+            category: 'Main'
         },
         {
             id: 2,
             name: 'test2',
             price: '2.00',
-            Category: 'Snack'
+            category: 'Snack'
         },
         {
             id: 3,
             name: 'test3',
             price: '3.33',
-            Category: 'Breakfast'
+            category: 'Breakfast'
         },
         {
             id: 4,
             name: 'test4',
             price: '4.44',
-            Category: 'Lunch'
+            category: 'Lunch'
         },
         {
             id: 5,
             name: 'test5',
             price: '5.55',
-            Category: 'Main'
+            category: 'Main'
         }
     ];
+console.log(process.env.DB_URL)
+    before(() => {
+        db = knex({
+          client: 'pg',
+          connection:'postgresql://dunder-mifflin:paper@localhost/knex-practice' ,
+        })
+      })
 
     before(() => db('shopping_list').truncate())
 
@@ -48,10 +56,10 @@ describe('shopping list service object', function() {
                 .into('shopping_list')
                 .insert(testItems)
             })
-        })
+        //})
 
         it(`getAllItems() returns all items from 'shopping_list' table`, () => {
-            const expected = testItems.map(item => ({...item}))
+            const expected = testItems.map(item => ({...item, checked:false}))
             return ShoppingListService.getAllItems(db)
                 .then(actual => {
                     expect(actual).to.eql(expected);
@@ -59,20 +67,68 @@ describe('shopping list service object', function() {
         } )
 
         it(`deleteItem() removes item by id from 'shopping_list' table`, () => {
-
-            return
+            let targetId = 2
+            return ShoppingListService.deleteItem(db,targetId)
+                    .then(() => ShoppingListService.getAllItems(db))
+                    .then(allItems => {
+                        const expected = testItems.filter(item => item.id !== targetId).map(item => ({...item, checked:false}))
+                        expect(allItems).to.eql(expected)
+                    })
         })
 
         it(`updateItem() updates item by id with new fields from 'shopping_list' table`, () => {
-
-            return
+                const targetId = 3;
+                const originalItem = testItems[targetId - 1]
+                const newFields = {
+                    name: 'updateTest',
+                    price: '9.99',
+                    checked: true,
+                    category: 'Lunch',
+                }
+            return ShoppingListService.updateItem(db, targetId, newFields)
+                    .then(() => ShoppingListService.getById(db, targetId))
+                    .then(actual => {
+                        expect(actual).to.eql({
+                            id: 3,
+                            name: newFields.name,
+                            price: newFields.price,
+                            checked: newFields.checked,
+                            category: newFields.category
+                        })
+                    })
         })
-        it(`insertItem() inserts item with new specified id into 'shopping_list' table`, () => {
+        it(`getById() returns an item with the specified id from 'shopping_list' table` ,() => {
+            const targetId = 2;
+            const targetItem = testItems[targetId -1];
 
-            return
+            return ShoppingListService.getById(db,targetId)
+            .then(actual => {
+                expect(actual).to.eql({
+                    id: targetId,
+                    name: targetItem.name,
+                    price: targetItem.price,
+                    category: targetItem.category,
+                    checked: false,
+                })
+            })
         })
-        it(`getById() returns an item with the specified id from 'shopping_list' table` () => {
-
-            return
+    })
+    it(`insertItem() inserts item with new specified id into 'shopping_list' table`, () => {
+            const newItem = {
+                name: 'testadd',
+                price: '7.77',
+                checked: false,
+                category: 'Main'
+            }
+            return ShoppingListService.insertItem(db,newItem)
+            .then(actual => {
+                expect(actual).to.eql({
+                    id:1,
+                    name: newItem.name,
+                    price: newItem.price,
+                    checked: newItem.checked,
+                    category: newItem.category,
+                })
         })
+    })
 })
